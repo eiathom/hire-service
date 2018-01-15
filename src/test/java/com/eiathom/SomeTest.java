ab7e1c4ab794bd15e5f2ad6537b1355fa844511a
ithompson <ian.thompson@genomicsengland.co.uk>
2018-01-15 00:18:07 +0000

    [EIATHOM-1] Adding structure functionality test
    adds a class to run, and test package structure

---
+++ src/test/java/com/eiathom/SomeTest.java
@@ -0,0 +1,11 @@
+package com.eiathom;
+
+public class SomeTest {
+    public SomeTest() {
+    }
+    public static void main(final String[] args) {
+        System.out.println(args.length);
+        System.out.println("Hello, world!");
+    }
+}
+
